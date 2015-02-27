3d4634d962ccc3215ee7ce46ce3ede5110259f5c
Nate Gordon <nlgordon@gmail.com>
2015-02-27 13:30:44 -0600

    POC, hopefully not POS.

---
+++ site/helloworld/app/Http/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function() {
+	$tests = App\TestModel::all();
+	// $tests = TestModel::all();
+	return 'hello world: ' . print_r($tests, 1);
+});
+
+Route::get('home', function() {
+	return 'hello world home';
+});
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
---
+++ site/helloworld/models/TestModel.php
@@ -0,0 +1,11 @@
+<?php
+
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestModel extends Model {
+
+	//
+
+}
