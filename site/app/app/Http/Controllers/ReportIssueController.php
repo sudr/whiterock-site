5b865a451a37589e5e58b1d39211226d00822d24
Jason Flogel <jason.flogel@gmail.com>
2015-02-27 17:43:36 -0600

    wiring up public user report issue page

---
+++ site/app/app/Http/Controllers/ReportIssueController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Http\Controllers;
+
+class ReportIssueController extends Controller {
+
+	public function getIndex()
+	{
+		return view('report-issue');
+	}
+
+}
---
+++ site/app/resources/views/report-issue.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+  <h3>report issue stuff here</h3>
+@stop
