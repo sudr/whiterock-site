ee18cf7d40d1d4327bb4380ecd755c091fd48896
Jason Flogel <jason.flogel@gmail.com>
2015-02-28 09:56:29 -0600

    created form/controller for updating issues

--- site/app/resources/views/issue/detail.blade.php
+++ site/app/resources/views/issue/detail.blade.php
@@ -2,31 +2,6 @@
 
 @section('content')
 
-<p>
-	{{$issue->resolved}}
-	<a href="{{ action('Issue\ManageController@getIndex') }}">List</a>
-@if ($issue->resolved)
-	<a href="{{ action('Issue\ManageController@getReopen', ['id' => $issue->id]) }}">Reopen</a>
-@endif
-@if (!$issue->resolved)
-	<a href="{{ action('Issue\ManageController@getMarkCompleted', ['id' => $issue->id]) }}">Completed</a>
-@endif
-
-<div>Name: {{ $issue->name}}</div>
-<div>Email: {{ $issue->email}}</div>
-<div>Phone: {{ $issue->phone}}</div>
-updatable!!!<div>Assigned On: {{ $issue->assigned}}</div>
-<div>Location: {{ $issue->location}}</div>
-<div>Type: {{ $issue->type}}</div>
-updatable!!!<div>Status: {{ $issue->status}}</div>
-updatable!!!<div>Priority: {{ $issue->priority}}</div>
-updatable!!!<div>Assigned To: {{ $issue->assigned_to}}</div>
-updatable!!!<div>Comments: {{ $issue->comment}}</div>
-<div>Resolved On: {{ $issue->resolved}}</div>
-<div>Trail: {{ $issue->trail_id}}</div>
-<div>Photo: {{ $issue->photo_id}}</div>
-<div>Description: {{ $issue->description}}</div>
-</p>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
@@ -44,42 +19,100 @@ updatable!!!<div>Comments: {{ $issue->comment}}</div>
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+					<form class="form-horizontal" role="form" method="POST" action="{{ action('Issue\ManageController@postEdit', ['id' => $issue->id]) }}" novalidate>
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<div class="form-group">
+							<label class="col-md-4 control-label">Description</label>
+							<div class="col-md-6">{{ $issue->description}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Location</label>
+							<div class="col-md-6">{{ $issue->location}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Trail</label>
+							<div class="col-md-6">{{ $issue->trail_id}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Photo</label>
+							<div class="col-md-6">{{ $issue->photo_id}}</div>
+						</div>
 
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
+							<div class="col-md-6">{{ $issue->name }}</div>
 						</div>
 
 						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
+							<div class="col-md-6">{{$issue->email}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Phone</label>
+							<div class="col-md-6">{{ $issue->phone}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Type</label>
+							<div class="col-md-6">{{ $issue->type}}</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Assigned Date</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="text" class="form-control" name="assigned" value="{{ $issue->assigned}}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label class="col-md-4 control-label">Status</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								<input type="text" class="form-control" name="status" value="{{ $issue->status}}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
+							<label class="col-md-4 control-label">Priority</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+								<input type="text" class="form-control" name="priority" value="{{ $issue->priority}}">
 							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Assigned To</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="assigned_to" value="{{ $issue->assigned_to}}">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Comment</label>
+							<div class="col-md-6">
+								<textarea class="form-control" rows="3" name="comment">{{ $issue->comment}}</textarea>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Resolved On</label>
+							<div class="col-md-6">{{ $issue->resolved ?: 'Not Resolved' }}</div>
 						</div>
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
+								<button type="submit" class="btn btn-primary">Save</button>
+								<a href="{{ action('Issue\ManageController@getIndex') }}">Cancel</a>
+								<div class="pull-right">
+								@if ($issue->resolved)
+									<a href="{{ action('Issue\ManageController@getReopen', ['id' => $issue->id]) }}">Reopen</a>
+								@endif
+								@if (!$issue->resolved)
+									<a href="{{ action('Issue\ManageController@getMarkCompleted', ['id' => $issue->id]) }}">Completed</a>
+								@endif
+								</div>
 							</div>
 						</div>
 					</form>
